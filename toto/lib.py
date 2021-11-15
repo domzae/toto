11cd4aab45c3a00c96fccea05b0555a955265446
Dominik Wagner <dominik.r.mall2@gmail.com>
2021-11-15 10:07:48 +0100

    set up CI

--- toto/lib.py
+++ toto/lib.py
@@ -1,3 +1,12 @@
+from termcolor import colored
+
+def whats_my_name():
+
+    return "Hello my name is Dom"
+
 def who_am_i():
 
-    print("Hello my name is Jean")
+    name = whats_my_name()
+
+    # difficult to test since the function returns None implicitly
+    print(colored(name, "blue"))
